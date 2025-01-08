2f182616a603ac464ad84cec85f136868dab8ae3
ckdev88 <ckdev88@outlook.com>
2025-01-08 11:30:48 -0300

    setup editorconfig & prettier with latest flow

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+	transpileDependencies: true,
+	__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
 })
